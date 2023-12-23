03c8fe82aa221b16a15eefada81e847865eb8356
Tanmay Singhal <43653881+tan-june@users.noreply.github.com>
2023-12-23 10:49:33 -0500

    data check mods

--- client/src/components/StartScreen.js
+++ client/src/components/StartScreen.js
@@ -13,6 +13,28 @@ class StartScreen extends React.Component {
     this.firstRender();
   }
 
+  dataCheck = (statusCode) => {
+    if(statusCode === 1){
+      const usernameoremail = document.getElementById('logina');
+      const password = document.getElementById('loginb');
+      if((usernameoremail.length > 5) && (password.length > 5)){
+        //axios.query
+        this.props.changeScreen('homePage');
+      }
+      else if (statusCode === 2){
+        const name = document.getElementById('name');
+        const username = document.getElementById('username');
+        const email = document.getElementById('email');
+        const password1 = document.getElementById('1pass');
+        const password2 = document.getElementById('2pass');
+        
+        // if( (name.length !== 0) && (username.length !== 0) && (email.isEmail))
+        //axios query
+        this.props.changeScreen('homePage');
+      }
+    }
+  }
+
   firstRender = () => {
     this.setState({
       content: (
@@ -45,14 +67,13 @@ class StartScreen extends React.Component {
             <h2 style={{ color: '#ec3f62', fontSize: '1.2em', textAlign: 'center' }}>Login</h2>
 
             <form action="" className="form">
-              <input type="text" placeholder="Username" className="form__input" id="username" />
-              <input type="password" placeholder="Password" className="form__input" id="1pass" />
+              <input type="text" placeholder="Username/Email*" className="form__input" id="logina" required/>
+              <input type="password" placeholder="Password*" className="form__input" id="loginb" required/>
             </form>
-
             <button className="button-62" onClick={this.firstRender}>
               Cancel
             </button>
-            <button className="button-62">Login</button>
+            <button className="button-62" onClick={this.dataCheck(1)}>Login</button>
           </div>
         </div>
       ),
@@ -68,11 +89,11 @@ class StartScreen extends React.Component {
           <h1 style={{ fontSize: '2 em', textAlign: 'center' }}>Instagram Clone</h1>
           <h2 style={{ color: '#ec3f62', fontSize: '1.2em', textAlign: 'center' }}>Sign Up</h2>
           <form action="" className="form">
-            <input type="text" placeholder="Name" className="form__input" id="name" />
-            <input type="text" placeholder="Username" className="form__input" id="username" />
-            <input type="email" placeholder="Email" className="form__input" id="email" />
-            <input type="password" placeholder="Password" className="form__input" id="1pass" />
-            <input type="password" placeholder="Confirm Password" className="form__input" id="2pass" />
+            <input type="text" placeholder="Name*" className="form__input" id="name" required/>
+            <input type="text" placeholder="Username*" className="form__input" id="username" required/>
+            <input type="email" placeholder="Email*" className="form__input" id="email" required/>
+            <input type="password" placeholder="Password*" className="form__input" id="1pass" required/>
+            <input type="password" placeholder="Confirm Password*" className="form__input" id="2pass" required/>
           </form>
           <br />
           <button className="button-62" onClick={this.firstRender}>
